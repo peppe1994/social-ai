a5cc4fe255457f7977c8fe49ee84eac4d6b527e3
peppe1994 <giuseppecuzzola94@gmail.com>
2024-10-14 21:06:29 +0200

    navbar components

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+export default {
+    dialect: 'postgresql',
+    schema: './utils/db/schema.ts',
+    out: './drizzle',
+    dbCredentials: {
+        url: 'postgresql://social-ai_owner:eBC4gHMSr8Il@ep-round-term-a508k530.us-east-2.aws.neon.tech/social-ai?sslmode=require',
+        connectionString: 'postgresql://social-ai_owner:eBC4gHMSr8Il@ep-round-term-a508k530.us-east-2.aws.neon.tech/social-ai?sslmode=require'
+    }
+}
